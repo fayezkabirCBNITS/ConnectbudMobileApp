7ef84b052e26cbc1532d4954307e0e9d7d1aba92
RKCbnits <ravindra.kumar@cbnits.com>
2020-12-15 19:13:48 +0530

    edit profile

--- src/screens/splash.js
+++ src/screens/splash.js
@@ -13,7 +13,8 @@ class SplashScreen extends Component {
   /**
    * Reset Navigation stack with a new route
    */
-  resetStack = (routeName = 'NonAuthStackNav') => {
+  //NonAuthStackNav
+  resetStack = (routeName = 'AuthStackNav') => {
     this.props.navigation.dispatch(
       StackActions.reset({
         index: 0,
