038951355521d419986fc9e63e5f87fab44b0e59
Angad <angad.kumar@cbnits.com>
2020-12-17 01:32:03 +0530

    Searcg student and post a project page added

--- src/components/SearchClgStu/index.js
+++ src/components/SearchClgStu/index.js
@@ -1,27 +1,134 @@
-import React, {Component} from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, FlatList, TouchableOpacity, Image, SafeAreaView } from 'react-native';
 import CommonStyles from '../../../CommonStyles';
-import {ScrollView} from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Entypo from "react-native-vector-icons/Entypo";
+import styles from './styles'
 
 class SearchClgStu extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      showSkills: false,
+      skillsData: [
+        { title: 'C' },
+        { title: 'JAVA' },
+        { title: 'C++' },
+        { title: 'C#' },
+      ],
+    };
   }
+
+  handleSkills = async () => {
+    this.setState({ showSkills: !this.state.showSkills });
+  };
 
   static navigationOptions = {
     headerShown: false,
   };
 
   render() {
+    const renderSkillItems = ({ item }) => (
+      <TouchableOpacity style={styles.headSec}>
+        <View style={styles.details}>
+          <Text style={styles.flastListHead}>{item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
     return (
-      <View style={CommonStyles.main}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View>
-            <Text>Search College Students</Text>
+      <SafeAreaView style={CommonStyles.safeAreaView}>
+        <View style={CommonStyles.main}>
+
+          <Text style={styles.title}>Search</Text>
+          <View style={styles.skillView}>
+            <View style={[styles.formGroup1]}>
+              <View style={[styles.formSubGroup2, { flexWrap: 'wrap' }]}>
+                <TextInput
+                  returnKeyType="done"
+                  placeholder="Java"
+                  style={styles.inputGroup}
+                  keyboardType="default"
+                />
+              </View>
+              <View style={{ marginTop: 10 }}>
+                <FontAwesome
+                  name="angle-down"
+                  size={28}
+                  color="#d7d7d8"
+                  onPress={() => this.handleSkills()}
+                />
+              </View>
+            </View>
           </View>
-        </ScrollView>
-      </View>
+          {this.state.showSkills === true ? (
+            <View style={[styles.flatList, { marginTop: -15 }]}>
+              <FlatList
+                data={this.state.skillsData}
+                ItemSeparatorComponent={this.FlatListItemSeparator}
+                renderItem={renderSkillItems}
+                showsHorizontalScrollIndicator={false}
+              />
+            </View>
+          ) : (
+              <></>
+            )}
+          <Text style={styles.title}>College Student List</Text>
+          <TouchableOpacity style={{ marginTop: -5 }}>
+            <View style={CommonStyles.container}>
+              <View style={styles.subjectWrapper}>
+                <View style={[styles.leftSection]}>
+                  <Image
+                    source={require('../../assets/images/userPro.jpg')}
+                    style={styles.userImg}
+                  />
+                </View>
+                <View style={styles.rightSection}>
+                  <Text style={styles.boxTitle}>Angad Kumar</Text>
+                  <View style={[styles.flexstyle, styles.timeAgo]}>
+
+                    <FontAwesome
+                      name="graduation-cap"
+                      color="#71b85f"
+                      size={15}
+                    />
+                    <Text style={styles.iconText}>Institute :<Text> San Jose</Text></Text>
+                  </View>
+                  <View style={[styles.flexstyle, styles.timeAgo]}>
+
+                    <FontAwesome
+                      name="institution"
+                      color="#71b85f"
+                      size={15}
+                    />
+                    <Text style={styles.iconText}>Prefered :<Text> Data Science</Text></Text>
+                  </View>
+                  <View style={[styles.flexstyle, styles.timeAgo, { marginLeft: -5, }]}>
+                    <Entypo
+                      name="location-pin"
+                      color="#71b85f"
+                      size={25}
+                    />
+                    <Text style={styles.iconText}>Location :<Text> Kolkata, India</Text></Text>
+                  </View>
+                  <View style={styles.btnGrp}>
+                    <TouchableOpacity style={styles.subBtn}>
+                      <Text style={styles.btnText}>Python</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.subBtn}>
+                      <Text style={styles.btnText}>Hive</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.subBtn}>
+                      <Text style={styles.btnText}>React</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+        </View>
+      </SafeAreaView>
     );
   }
 }
--- src/components/SearchClgStu/styles.js
+++ src/components/SearchClgStu/styles.js
@@ -1,6 +1,124 @@
 const React = require('react-native');
-const {Dimensions} = React;
+const { Dimensions } = React;
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
 
-export default {}
+export default {
+    skillView: {
+        width: '100%',
+        height: 60,
+        alignContent: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        marginHorizontal: '5%',
+        marginTop: '2%'
+    },
+    formGroup1: {
+        width: '90%',
+        marginBottom: 15,
+        backgroundColor: '#f8f8f8',
+        borderRadius: 4,
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: 'rgba(59,29,37,0.5)',
+        flexDirection: 'row',
+    },
+    formSubGroup2: {
+        width: '90%',
+    },
+    flastListHead: {
+        color: 'rgba(59,29,37,0.5)',
+        fontSize: 14,
+        marginBottom: 5,
+    },
+    flatList: {
+        width: '90%',
+        paddingHorizontal: 5,
+        marginHorizontal: '5%',
+        backgroundColor: '#f8f8f8',
+        borderRadius: 4,
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: 'rgba(59,29,37,0.5)',
+        flexDirection: 'row',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 14,
+        marginLeft: 20,
+        marginTop: 10,
+    },
+    popins: {
+        fontFamily: "Poppins"
+    },
+    flexstyle: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    subjectWrapper: {
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        width: "100%",
+        borderColor: "rgba(113,184,95,0.3)",
+        borderWidth: 2,
+        marginVertical: 15,
+        padding: 15,
+        borderRadius: 6
+    },
+    moneyContainer: {
+        borderColor: "rgba(113,184,95,0.5)",
+        borderWidth: 2,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        overflow: "hidden",
+        borderRadius: 5,
+        marginTop: 15
+    },
+    userImg: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginTop: -72
+    },
+    timeAgo: {
+        marginBottom: 5,
+
+    },
+    rightSection: {
+        width: "78%",
+        paddingLeft: 5
+    },
+    leftSection: {
+        width: "22%"
+    },
+    boxTitle: {
+        fontSize: 15,
+        fontFamily: 'Poppins-SemiBold',
+    },
+    iconText: {
+        marginLeft: 10,
+        fontSize: 12,
+        fontFamily: 'Poppins-Bold',
+        color: 'rgba(59,29,37,0.5)',
+    },
+    btnGrp:{
+        display: 'flex', 
+        flexDirection: 'row', 
+        marginTop: 10
+    },
+    subBtn:{
+        backgroundColor: '#E4E4E4', 
+        borderRadius: 40, 
+        width: '30%', 
+        alignItems: 'center', 
+        paddingHorizontal: 10, 
+        paddingVertical: 5, 
+        marginHorizontal: 3
+    },
+    btnText:{
+        color: 'rgba(59,29,37,0.5)',
+    },
+
+}
