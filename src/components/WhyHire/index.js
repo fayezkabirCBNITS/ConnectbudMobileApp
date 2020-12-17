95750fcafe24e5ffc3ab2e09f5951a1ba5bb0d31
Swarup Chakraborty <ndali.cbnits@gmail.com>
2020-12-17 18:32:26 +0530

    home screen new section added

---
+++ src/components/WhyHire/index.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
+import CommonStyles from '../../../CommonStyles';
+import {ScrollView} from 'react-native-gesture-handler';
+import styles from './styles';
+
+class WhyHire extends Component {
+  constructor() {
+    super();
+    this.state = {
+      whyHire: [
+        {
+          head: 'Cost-effective',
+          details: '25% of cost of a regular cost',
+          img: require('../../assets/images/cost.png'),
+        },
+        {
+          head: 'Authenticated',
+          details: 'Verified and curated College Students',
+          img: require('../../assets/images/auth.png'),
+        },
+        {
+          head: 'Qualified',
+          details: 'Well educated and up-to date with current trends',
+          img: require('../../assets/images/qualified.png'),
+        },
+        {
+          head: 'Committed',
+          details: 'Motivated to excel the assignments',
+          img: require('../../assets/images/committed.png'),
+        },
+        {
+          head: 'Effective',
+          details: 'Super energetic and goal oriented',
+          img: require('../../assets/images/effective.png'),
+        },
+      ],
+    };
+  }
+
+  static navigationOptions = {
+    headerShown: false,
+  };
+
+  render() {
+    return (
+      <View>
+        <ScrollView showsHorizontalScrollIndicator={false} horizontal>
+          {this.state.whyHire.map((item, i) => (
+            <View key={i} style={styles.width200}>
+              <View style={styles.imgSec}>
+                <Image
+                  source={item.img}
+                  style={styles.image}
+                />
+              </View>
+              <View style={{alignItems: 'center'}}>
+                <Text style={styles.headText}>{item.head}</Text>
+                <Text style={styles.detailsText}>
+                  {item.details}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+export default WhyHire;
---
+++ src/screens/AuthNav/EmployeeProfile/index.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import CommonStyles from '../../../CommonStyles';
+import {ScrollView} from 'react-native-gesture-handler';
+
+class EmployeeProfile extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  static navigationOptions = {
+    headerShown: false,
+  };
+
+  render() {
+    return (
+      <View style={CommonStyles.main}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View>
+            <Text>Post InternShip</Text>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+export default EmployeeProfile;
